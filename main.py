96046201f98a8e639756abcfed01f587fa5f26c0
Regin Kunoy <59647732+RKunoy@users.noreply.github.com>
2022-11-05 16:31:07 +0100

    Fixed bugs. Does now function as intended

--- main.py
+++ main.py
@@ -26,31 +26,33 @@ class Gate:
 
 
 def luggage(env, id, gate):
-    global loaded_luggage
     print(f"Luggage (id: {id}) ready for loading. {env.now:.2f}")
     with gate.robot.request() as request:
         yield request
         print(f"Luggage {id} is being loaded. {env.now:.2f}")
         yield env.process(gate.load(id))
         print(f"Luggage (id: {id}) has been loaded. {env.now:.2f}")
-        loaded_luggage += 1
 
 
-def setup(env, num_robots, num_luggage, loading_time):
+def load_simulation(env, num_robots, num_luggage, loading_time):
     gate = Gate(env, num_robots, num_luggage, loading_time)
 
-    for i in range(1, num_luggage+1):
-        env.process(luggage(env, i, gate))
+    # This for-loop populates the gate with luggage before running
+    # Make the starting luggage count be equal to the amount of robots so each robot is active when program starts
+    robot_id = 1
+    for robot_id in range(1, num_robots+1):
+        env.process(luggage(env, robot_id, gate))
 
-    while True:
+    # this line creates more luggages after the simulation has started
+    while robot_id <= num_luggage:
         yield env.timeout(random.randint(loading_time - 1, loaded_luggage + 1))
-        i += 1
-        env.process(luggage(env, i, gate))
+        env.process(luggage(env, robot_id, gate))
+        robot_id += 1
 
 
 print("Starting Luggage loading simulation")
 env = simpy.Environment()
-env.process(setup(env, NUM_ROBOTS, NUM_LUGGAGE, AVG_LOADING_TIME))
-env.run(until=SIMULATION_TIME)
+env.process(load_simulation(env, NUM_ROBOTS, NUM_LUGGAGE, AVG_LOADING_TIME))
+env.run()
 
 print("Luggage loaded: ", loaded_luggage)
